dc805862c091938ddd2da205dd7069928167bb2c
encokarthur298 <endev1569@gmail.com>
2025-06-23 12:11:48 +0100

    made update

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -26,7 +26,7 @@ export default function Home() {
            <FeatureSection />
           {/* <GrowthSection /> */}
           {/*<OpenSourceSection />*/}
-          {/*<PricingSection />*/}
+          *<PricingSection />
           {/* <TestimonialSection /> */}
            <FAQSection/>
           <CTASection />
